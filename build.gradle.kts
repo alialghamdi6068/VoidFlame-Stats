81c946e3c1b4f8e29bc08376a217625d84c30b0b
alialghamdi6068 <alghamdiali6068@gmail.com>
2026-09-25 20:08:20 +0300

    Initialize VoidFlame-Stats

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    java
+    id("com.gradleup.shadow") version "9.0.0"
+}
+group = "net.voidflame"
+version = "1.0.0"
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
+}
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+    withSourcesJar()
+}
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    options.release.set(25)
+}
+tasks.jar { archiveBaseName.set(project.name) }
+tasks.shadowJar { archiveBaseName.set(project.name); archiveClassifier.set("") }
+tasks.build { dependsOn(tasks.shadowJar) }
