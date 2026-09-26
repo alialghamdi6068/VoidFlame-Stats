78032366cc33c9179b0ac68d416b817ca3d83692
alialghamdi6068 <alghamdiali6068@gmail.com>
2026-09-26 14:23:07 +0300

    Configure Core API source dependency

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "VoidFlame-Stats"
+
+sourceControl {
+    gitRepository(uri("https://github.com/alialghamdi6068/VoidFlame-Core.git")) {
+        producesModule("net.voidflame:VoidFlame-Core")
+    }
+}
